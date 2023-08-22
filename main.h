6c636d8cc5d81f586e4ad259bc7cd03f6431dc8c
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-22 14:42:48 +0300

    Header file

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
+
+int get_char(const char *, va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
