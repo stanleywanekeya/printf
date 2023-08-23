320b65aa717615874f87aa5fc1663ad8c454b201
Daniel Musau <136097805+Dmusau@users.noreply.github.com>
2023-08-23 16:54:54 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
+ * _printf - Receives all the main string and all the necessary parameters to
  * print a formated string
  * @format: A string containing all the desired characters
  * Return: A total count of the characters printed
