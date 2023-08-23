0749e3615a7cb5ff8a360d81b4af9d299b95fd07
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-23 06:28:31 +0300

    get_format

--- format.c
+++ format.c
@@ -1,15 +1,14 @@
-#include "main.h"
 
 /**
- * get_format - Receives the main string and all the
+ * format_reciever - Receives the main string and all the
  * necessary parameters to print a formated string.
  * @format: A string containing all the desired characters.
- * @list: A list of all the posible functions.
- * @ap_List: A list containing all the argumentents passed to the program.
+ * @f_list: A list of all the posible functions.
+ * @arg_list: A list containing all the argumentents passed to the program.
  * Return: A total count of the characters printed.
  */
 
-int get_format(const char *format, conver_t list[], va_list ap_List)
+int format_reciever(const char *format, conver_t f_list[], va_list arg_list)
 {
 	int i, j, r_val, printed_chars;
 
@@ -18,18 +17,18 @@ int get_format(const char *format, conver_t list[], va_list ap_List)
 	{
 		if (format[i] == '%')
 		{
-			for (j = 0; list[j].sym != NULL; j++)
+			for (j = 0; f_list[j].sym != NULL; j++)
 			{
-				if (format[i + 1] == list[j].sym[0])
+				if (format[i + 1] == f_list[j].sym[0])
 				{
-					r_val = list[j].f(ap_List);
+					r_val = f_list[j].f(arg_list);
 					if (r_val == -1)
 						return (-1);
 					printed_chars += r_val;
 					break;
 				}
 			}
-			if (list[j].sym == NULL && format[i + 1] != ' ')
+			if (f_list[j].sym == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
@@ -40,7 +39,7 @@ int get_format(const char *format, conver_t list[], va_list ap_List)
 				else
 					return (-1);
 			}
-			++i;
+			i = i + 1;
 		}
 		else
 		{
