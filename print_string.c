f61bb3c85af0a48cfd7bdb3acbb450fe41d0d7e3
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-23 06:43:04 +0300

    string

--- print_string.c
+++ print_string.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * print_string - Prints a string
  * @list: list of arguments
  * Return: Will return the amount of characters printed.
  */
-
 int print_string(va_list list)
 {
 	int i;
 	char *str;
-
+	
 	str = va_arg(list, char *);
 	if (str == NULL)
 		str = "(null)";
