4dbfb6fbdbc2b2d90cbdbddb664926283fa919a6
stanleywanekeya <Stanleywanekeya123@gmail.com>
2023-08-23 08:05:41 +0300

    print

--- print_String.c
+++ print_String.c
@@ -2,7 +2,7 @@
 
 /**
  * print_String - print exclusuives string.
- * @val: argumen t.
+ * @l: argumen t.
  * Return: the length of the string.
  */
 
@@ -37,7 +37,7 @@ int print_String(va_list l)
  * Return: true or false
  */
 
-int isNonAlphaNumeric(char c) 
+int isNonAlphaNumeric(char c)
 {
 	return ((c > 0 && c < 32) || c >= 127);
 }
