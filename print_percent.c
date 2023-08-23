93d8a9b27afc1073bf75ff0ab5a48b1e0dd1a624
Daniel Musau <136097805+Dmusau@users.noreply.github.com>
2023-08-23 17:04:35 +0300

    Update print_percent.c

--- print_percent.c
+++ print_percent.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_percent - Prints a percent symbol
+ * print_percent - Print a percent symbol
  * @list: list of arguments
  * Return: Will return the amount of characters printed.
  */
